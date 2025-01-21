6d4d5cbf56e689cbd4f880f6d131938a310f4016
wukakuki <7627497+wukakuki@users.noreply.github.com>
2025-01-21 22:24:16 +0800

    ready for player customization system

---
+++ Source/awsTutorial/awsTutorial_PlayerController.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Siqi Wu on 1/17/25.
+//
+
+#include "awsTutorial_PlayerController.h"
+
+FString AAwsTutorial_PlayerController::GetRequestURL() const {
+	UNetConnection* netConnection = GetNetConnection();
+	if (netConnection == NULL) return FString();
+	return netConnection->RequestURL;
+}
+
+TArray<FString> AAwsTutorial_PlayerController::GetRequestOptions() const {
+	UNetConnection* netConnection = GetNetConnection();
+	if (netConnection == NULL) return TArray<FString>();
+	FURL InURL( NULL, *netConnection->RequestURL, TRAVEL_Absolute );
+	return InURL.Op;
+}
+
+bool AAwsTutorial_PlayerController::HasRequestOption(const FString& key) const {
+	UNetConnection* netConnection = GetNetConnection();
+	if (netConnection == NULL) return false;
+	FURL InURL( NULL, *netConnection->RequestURL, TRAVEL_Absolute );
+	return InURL.HasOption(*key);
+}
+
+FString AAwsTutorial_PlayerController::GetRequestOption(const FString& key) const {
+	UNetConnection* netConnection = GetNetConnection();
+	if (netConnection == NULL) return FString("");
+	FURL InURL( NULL, *netConnection->RequestURL, TRAVEL_Absolute );
+	const TCHAR* o = InURL.GetOption(*key, NULL);
+	if (o == NULL) return FString("");
+	if (o[0] == '=') return FString(o + 1);
+	return FString(o);
+}
---
+++ Source/awsTutorial/awsTutorial_PlayerController.h
@@ -0,0 +1,29 @@
+//
+// Created by Siqi Wu on 1/17/25.
+//
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/PlayerController.h"
+
+#include "awsTutorial_PlayerController.generated.h"
+
+UCLASS(Blueprintable)
+class AWSTUTORIAL_API AAwsTutorial_PlayerController : public APlayerController {
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="awsTutorial")
+	FString GetRequestURL() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="awsTutorial")
+	TArray<FString> GetRequestOptions() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="awsTutorial")
+	bool HasRequestOption(const FString& key) const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="awsTutorial")
+	FString GetRequestOption(const FString& key) const;
+};
